09282b7f5146a0dae2787a8b5dee4680631a2986
Yenyun035 <yew005@ucsd.edu>
2021-07-09 07:12:55 +0000

    todo_write

--- backend/src/routes/todo.ts
+++ backend/src/routes/todo.ts
@@ -13,6 +13,12 @@ const TodoRouter = (server: FastifyInstance, opts: RouteShorthandOptions, done: 
     // TODO: Add CRUD endpoints, i.e. get, post, update, delete
     // NOTE: the url should be RESTful
 
+    server.addTodo('/todos', opts, async(request, reply) => {
+        const todoBody: ITodo = request.body as ITodo
+        const todo: ITodo = await todoRepo.addTodo(todoBody)
+        return reply.status(201).send({todo})
+    })
+
     done()
 }
 
--- backend/src/test/todo.spec.ts
+++ backend/src/test/todo.spec.ts
@@ -34,4 +34,20 @@ describe('Form test', () => {
     })    
 
     // TODO: Add some test cases like CRUD, i.e. get, post, update, delete
+    it('should get todo items after input', async() => {
+        const response = await server.inject( {
+            method: 'POST',
+            url: '/api/todos',
+            payload: {
+                name: 'clean my desk',
+                description: 'hhh',
+                status: false
+            }
+        })
+
+        expect(response.statusCode).toBe(201)
+        const res: { todo: ITodo} = JSON.parse(response.body)
+        expect(res.todo.name).toBe('clean my desk')
+
+    })
 })
