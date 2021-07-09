405f1a51c97fa0455846c665970d4f178e3b390e
Yenyun035 <yew005@ucsd.edu>
2021-07-09 07:47:27 +0000

    add tests

--- backend/src/routes/todo.ts
+++ backend/src/routes/todo.ts
@@ -13,10 +13,33 @@ const TodoRouter = (server: FastifyInstance, opts: RouteShorthandOptions, done: 
     // TODO: Add CRUD endpoints, i.e. get, post, update, delete
     // NOTE: the url should be RESTful
 
-    server.addTodo('/todos', opts, async(request, reply) => {
-        const todoBody: ITodo = request.body as ITodo
-        const todo: ITodo = await todoRepo.addTodo(todoBody)
-        return reply.status(201).send({todo})
+    server.post('/todos', opts, async(request, reply) => {
+        try {
+            const todoBody: ITodo = request.body as ITodo
+            const todo: ITodo = await todoRepo.addTodo(todoBody)
+            return reply.status(201).send({todo})
+        } catch(error) {
+            return reply.status(500).send('Server error...')
+        }
+    })
+
+    server.put<{Params: IdParam}>('/todos', opts, async(request, reply) => {
+        try {
+            const id = request.params.id;
+            const todoBody: ITodo = request.body as ITodo
+            /**
+             * todoBody {
+             *  name: 'clean my desk',
+             *  description: 'hhh',
+             *  status: true
+             * }
+             */
+            
+            const todo: ITodo | null = await todoRepo.updateTodo(id,todoBody)
+            return reply.status(200).send({todo})
+        } catch(error) {
+            return reply.status(500).send('Server error...')
+        }
     })
 
     done()
